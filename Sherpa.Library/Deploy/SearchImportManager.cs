dce423eadfddd1b41a2e088e5c677d49c51cff4e
tarjeieo <ormestoyl@outlook.com>
2014-10-14 12:24:59 +0200

    Added support for setting welcome page and for importing search settings

---
+++ Sherpa.Library/Deploy/SearchImportManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Search.Administration;
+using Microsoft.SharePoint.Client.Search.Portability;
+
+namespace Sherpa.Library.Deploy
+{
+    public class SearchImportManager
+    {
+        public void ImportSearchConfiguration(ClientContext context, string pathToSearchXml)
+        {
+            var searchConfigurationPortability = new SearchConfigurationPortability(context);
+            var owningScope = new SearchObjectOwner(context, SearchObjectLevel.SPSite);
+
+            var configurationXml = new XmlDocument();
+            configurationXml.Load(pathToSearchXml);
+
+            searchConfigurationPortability.ImportSearchConfiguration(owningScope, configurationXml.OuterXml);
+            context.ExecuteQuery();
+        }
+    }
+}
