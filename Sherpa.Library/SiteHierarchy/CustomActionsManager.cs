6054d761837662e47bb0ef01c475dd0281914973
Ole Martin Pettersen <olemp@puzzlepart.com>
2015-08-05 10:50:05 +0200

    Fixed a bug in the custom actions manager

--- Sherpa.Library/SiteHierarchy/CustomActionsManager.cs
+++ Sherpa.Library/SiteHierarchy/CustomActionsManager.cs
@@ -13,63 +13,66 @@ namespace Sherpa.Library.SiteHierarchy
 
         public void SetUpCustomActions(ClientContext context, string CustomActionsPrefix, List<ShCustomAction> customActions)
         {
-            if (CustomActionsPrefix != null || CustomActionsPrefix.Equals(String.Empty))
+            if (customActions.Count > 0)
             {
-                Log.Info("You need to set the property 'CustomActionsPrefix' which will be used for the Custom Action Name.");
-                return; 
-            }
-
-            Log.Info("Adding custom actions");
-            Site site = context.Site;
-            context.Load(site);
-            context.Load(site.UserCustomActions);
-            context.ExecuteQuery();
-
-            for (var i = site.UserCustomActions.Count - 1; i >= 0; i--)
-            {
-                var customAction = site.UserCustomActions[i];
-                if (customAction.Name.StartsWith(CustomActionsPrefix))
+                if (CustomActionsPrefix == null || CustomActionsPrefix.Equals(String.Empty))
                 {
-                    customAction.DeleteObject();
+                    Log.Info("You need to set the property 'CustomActionsPrefix' which will be used for the Custom Action Name.");
+                    return;
                 }
-            }
 
-            if (context.HasPendingRequest)
-            {
+                Log.Info("Adding custom actions");
+                Site site = context.Site;
+                context.Load(site);
+                context.Load(site.UserCustomActions);
                 context.ExecuteQuery();
-            }
 
-            foreach (ShCustomAction customAction in customActions)
-            {
-                if (customAction.Location == null)
+                for (var i = site.UserCustomActions.Count - 1; i >= 0; i--)
                 {
-                    Log.Error("You need to specify a location for your Custom Action. Ignoring " + customAction.ScriptSrc);
-                    continue;
+                    var customAction = site.UserCustomActions[i];
+                    if (customAction.Name.StartsWith(CustomActionsPrefix))
+                    {
+                        customAction.DeleteObject();
+                    }
                 }
 
-                Log.DebugFormat("Adding custom action with src '{0}' at location '{1}'", customAction.ScriptSrc, customAction.Location);
-
-                UserCustomAction userCustomAction = site.UserCustomActions.Add();
-                userCustomAction.Location = customAction.Location;
-                userCustomAction.Sequence = customAction.Sequence;
-                userCustomAction.ScriptSrc = customAction.ScriptSrc;
-                userCustomAction.ScriptBlock = customAction.ScriptBlock;
-                userCustomAction.Name = CustomActionsPrefix + "_" + customAction.ScriptSrc.Split('/')[customAction.ScriptSrc.Split('/').Length - 1].Replace(".", "");
-                userCustomAction.Description = customAction.Description;
-                userCustomAction.RegistrationType = customAction.RegistrationType;
-                userCustomAction.Title = customAction.Title;
-                userCustomAction.ImageUrl = customAction.ImageUrl;
-                userCustomAction.Group = customAction.Group;
-                
-                try
+                if (context.HasPendingRequest)
                 {
-                    userCustomAction.Update();
                     context.ExecuteQuery();
-                    Log.Debug("Custom action successfully added.");
                 }
-                catch (Exception e)
+
+                foreach (ShCustomAction customAction in customActions)
                 {
-                    Log.Error(e.Message);
+                    if (customAction.Location == null)
+                    {
+                        Log.Error("You need to specify a location for your Custom Action. Ignoring " + customAction.ScriptSrc);
+                        continue;
+                    }
+
+                    Log.DebugFormat("Adding custom action with src '{0}' at location '{1}'", customAction.ScriptSrc, customAction.Location);
+
+                    UserCustomAction userCustomAction = site.UserCustomActions.Add();
+                    userCustomAction.Location = customAction.Location;
+                    userCustomAction.Sequence = customAction.Sequence;
+                    userCustomAction.ScriptSrc = customAction.ScriptSrc;
+                    userCustomAction.ScriptBlock = customAction.ScriptBlock;
+                    userCustomAction.Name = CustomActionsPrefix + "_" + customAction.ScriptSrc.Split('/')[customAction.ScriptSrc.Split('/').Length - 1].Replace(".", "");
+                    userCustomAction.Description = customAction.Description;
+                    userCustomAction.RegistrationType = customAction.RegistrationType;
+                    userCustomAction.Title = customAction.Title;
+                    userCustomAction.ImageUrl = customAction.ImageUrl;
+                    userCustomAction.Group = customAction.Group;
+
+                    try
+                    {
+                        userCustomAction.Update();
+                        context.ExecuteQuery();
+                        Log.Debug("Custom action successfully added.");
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e.Message);
+                    }
                 }
             }
         }
